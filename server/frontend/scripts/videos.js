d8b2903edbb0226526e625d17253e25ac74a9ae9
Till <ti.fricke@ostfalia.de>
2020-07-10 14:11:37 +0200

    -added videos.js

---
+++ server/frontend/scripts/videos.js
@@ -0,0 +1,12 @@
+class video {
+    constructor(channel,title,show,releaseDate,duration,link,pageLink,fileName) {
+        this.channel = channel;
+        this.title = title;
+        this.show = show;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+        this.link = link;
+        this.pageLink = pageLink;
+        this.fileName = fileName;
+    }
+}

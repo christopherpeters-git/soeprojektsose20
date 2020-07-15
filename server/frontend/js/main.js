91237f11a52b154f8ba0b68e6fb179c1a0fbb81c
mathusankann <61835302+mathusankann@users.noreply.github.com>
2020-07-15 12:50:15 +0200

    - added Senderpage - login function

--- server/frontend/js/main.js
+++ server/frontend/js/main.js
@@ -1,9 +1,85 @@
 
+"use strict"
+
+let username;
+let password;
+let favorites;
+
+class User{
+    constructor(id,name,username,favoriteVideos) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.favoriteVideos= favoriteVideos;
+    }
+
+}
+
+
+
+function sendPostLoginRequest(){
+    const usernameInput = document.getElementById("usernameInput").value;
+    const passwordInput = document.getElementById("passwordInput").value;
+    const request = createAjaxRequest();
+    request.onreadystatechange = function () {
+        if(4 === this.readyState){
+            if(200 === this.status){
+                alert(this.responseText)
+                let user = new User("","","",null);
+                user = JSON.parse(this.responseText);
+                username = usernameInput;
+                password = passwordInput;
+                favorites= user.FavoriteVideos;
+                setVisibilityLoginScreen();
+                
+            }else{
+                alert(this.status + ":" + this.responseText);
+            }
+        }
+    }
+    request.open("POST",/login/,true);
+    request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    console.log("" + usernameInput + " " + passwordInput);
+    request.send("usernameInput="+usernameInput+"&"+"passwordInput="+passwordInput);
+}
+
+function setVisibilityLoginScreen() {
+    let loginScreen = document.getElementById("Login_Screen");
+    let v_blocker = document.getElementById("v_blocker");
+    if(loginScreen.hidden==false){
+        loginScreen.hidden=true;
+        v_blocker.hidden= true;
+
+    }
+
+
+}
+
+function sendPostRegisterRequest(){
+    const name = document.getElementById("nameInput2").value;
+    const usernameInput = document.getElementById("usernameInput2").value;
+    const passwordInput = document.getElementById("passwordInput2").value;
+    const request = createAjaxRequest();
+    request.onreadystatechange = function () {
+        if(4 === this.readyState){
+            if(200 === this.status){
+                alert(this.responseText)
+
+            }else{
+                alert(this.status + ":" + this.responseText);
+            }
+        }
+    }
+    request.open("POST",/register/,true);
+    request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    console.log("" + usernameInput + " " + passwordInput);
+    request.send("usernameInput="+usernameInput+"&"+"passwordInput="+passwordInput+"&"+"nameInput="+name);
+}
 
 
 
 
-function open_L_R(evt, cityName) {
+function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
@@ -20,6 +96,6 @@ function open_L_R(evt, cityName) {
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
---
+++ server/frontend/js/sender.js
@@ -0,0 +1,4 @@
+function testLoad(value) {
+   window.open("/senderpage.html");
+   console.log(value);
+}

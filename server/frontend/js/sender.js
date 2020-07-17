3087c23d8b9b5550180ebf61f51acdbfb3a0be6e
mathusankann <61835302+mathusankann@users.noreply.github.com>
2020-07-17 13:53:10 +0200

    -added new functions

--- server/frontend/js/sender.js
+++ server/frontend/js/sender.js
@@ -2,7 +2,9 @@ let channelJson;
 let channelName;
 let start =0;
 let end = 30;
-let siteNumber;
+
+let currentPage =1;
+let lastPage =10;
 
 
 class Videoclass {
@@ -22,7 +24,6 @@ function loadSenderPage(wert) {
     window.location.href = "/senderpage.html";
     channelName = wert;
     sessionStorage.setItem('channel', wert);
-    sendGetVideos();
 
 }
 
@@ -34,9 +35,9 @@ function sendGetVideos() {
                 channelJson = JSON.parse(this.responseText);
                 channelName = sessionStorage.getItem("channel");
                 console.log(channelJson);
+                addButtons();
                 setPage();
-                siteNumber = channelJson.length/30;
-                console.log(siteNumber);
+
             } else {
                 alert(this.status + ":" + this.responseText);
             }
@@ -58,38 +59,84 @@ function createAjaxRequest() {
 }
 
 function setPage() {
-    console.log("start");
-    const videodiv = document.getElementById("videos");
+    const videosdiv = document.getElementById("videos");
     let currentVideo = new Videoclass("", "", "", "", "", "", "", "");
     let lastVideo;
-    lastVideo = channelJson[0];
-
-    let show =document.createElement("div");
+    let show =  document.createElement("div");
+    lastVideo = channelJson[start];
     show.id = lastVideo.show;
-    console.log(show);
-     for (let i = start; i < end; i++) {
-         currentVideo = channelJson[i];
-        if (currentVideo.show !== lastVideo.show) {
-           if(i>0) {
-              videodiv.appendChild(show);
-           }
-           show =  document.createElement("div");
-           show.id= currentVideo.show;
-           console.log(show);
+    appendShow(lastVideo,show);
+    for(let i =(start+1)+((currentPage-1)*end);i<end*currentPage;i++){
+        currentVideo = channelJson[i];
+        if(lastVideo.show !== currentVideo.show){
+            videosdiv.appendChild(show);
+            show =  document.createElement("div");
+            show.id =  currentVideo.show;
         }
-
-        let element = document.createElement("input");
-        element.id = currentVideo.title;
-        element.type = "image";
-        element.src = "media/Sender-Logos/zdf.png";
-        element.className = "sender"
-        element.value = JSON.stringify(currentVideo);
-        show.appendChild(element);
+        appendShow(currentVideo,show);
         lastVideo = currentVideo;
     }
-    videodiv.appendChild(show);
-    console.log("Ende");
-
+    videosdiv.appendChild(show);
 
 }
 
+function appendShow(video,showdiv){
+    const videoDiv = document.createElement("div");
+    const header5 = document.createElement("h5");
+    const header7 = document.createElement("h6");
+    const img = document.createElement("img");
+    const a = document.createElement("a");
+    a.href=JSON.stringify(video);
+    videoDiv.setAttribute("class","videoLink");
+    img.setAttribute("src","/media/Sender-Logos/ard.png");
+    img.setAttribute("class","thumbnail");
+    videoDiv.appendChild(a);
+    header5.innerHTML = video.title;
+    header7.innerHTML = video.duration;
+    videoDiv.appendChild(img);
+    videoDiv.appendChild(header5);
+    videoDiv.appendChild(header7);
+    showdiv.appendChild(videoDiv);
+}
+
+function addButtons() {
+    const buttonDiv = document.getElementById("buttons");
+    for(let i =currentPage; i<=lastPage;i++){
+        let button = document.createElement("button");
+        button.className= "senderPageButtons";
+        button.value = JSON.stringify(i);
+        button.textContent= JSON.stringify(i);
+        button.addEventListener('click',setPageWithNumbers,false);
+        buttonDiv.appendChild(button);
+    }
+}
+
+function setPageWithNumbers() {
+    setPageNumbers(this.value);
+    setPage();
+    let buttonSet;
+    buttonSet= document.getElementsByClassName("senderPageButtons");
+    console.log(buttonSet);
+    for(let i =0;i<=buttonSet.length;i++){
+        buttonSet[i].value = currentPage+i;
+        buttonSet[i].textContent = JSON.stringify(currentPage+i);
+    }
+}
+
+ async function setPageNumbers(value) {
+    currentPage = parseInt(value);
+    lastPage = currentPage + 10;
+    console.log(lastPage);
+    let videosDiv = document.getElementById("videos");
+    videosDiv.remove();
+    videosDiv = document.createElement("div");
+    videosDiv.id = "videos";
+    const vContainer = document.getElementById("videoContainer");
+    vContainer.appendChild(videosDiv);
+    console.log(videosDiv);
+     await new Promise((res, rej) => {
+         setTimeout(() => res("Now it's done!"), 300)
+     });
+
+    return 1;
+}

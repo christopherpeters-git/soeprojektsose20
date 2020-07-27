289224a0b8079245c225f8e3a23cb9130ffc5e87
Christopher Peters <chrissi.peters98@web.de>
2020-07-27 16:02:34 +0200

    changed cookie message

--- server/frontend/js/communication.js
+++ server/frontend/js/communication.js
@@ -62,7 +62,7 @@ function sendPostLoginRequest(callbackFunction){
     const usernameInput = document.getElementById("usernameLogin").value;
     const passwordInput = document.getElementById("passwordLogin").value;
     const stringArray = [usernameInput,passwordInput];
-    alert("Wir benutzen Cookies, um Inhalte und Anzeigen zu personalisieren.")
+    alert("Wir benutzen einen Cookie, um die automatische Authentifizierung zu ermöglichen.");
     if(areStringsIllegal(stringArray)){
         console.alert("Illegale strings");
         return;

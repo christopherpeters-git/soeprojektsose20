9389d3c911a56daef0295dea8f9c9caff0ae0ec9
Christopher Peters <chrissi.peters98@web.de>
2020-07-29 10:10:30 +0200

    changed comment #2

--- server/Server.go
+++ server/Server.go
@@ -543,7 +543,7 @@ func handlePostRegisterUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Created a new User account"))
+	w.Write([]byte("Neuer account angelegt"))
 	log.Println("answered handlePostRegisterUser request successfully")
 }
 
